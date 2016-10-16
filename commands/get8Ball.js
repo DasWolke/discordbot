badabbb0346bd9358139a4f7ec209e38436ccfee
Strawberry-Yukki <julianhannig@gmail.com>
2016-10-16 18:06:01 +0200

    Added a check in 8ball for messages

--- commands/get8Ball.js
+++ commands/get8Ball.js
@@ -6,7 +6,12 @@ var i18nBean = require('../utility/i18nManager');
 var t = i18nBean.getT();
 var general = require('../utility/general');
 var execute = function (message) {
-    var random = general.random(0, 7);
-    message.reply(t(`8ball.${random}`));
+    let content = message.content.substr(message.prefix.length + cmd.length).trim();
+    if (content && content !== '') {
+        var random = general.random(0, 7);
+        message.reply(t(`8ball.answers.${random}`, {lngs:message.lang}));
+    } else {
+        message.reply(t(`8ball.no-message`, {lngs:message.lang}));
+    }
 };
 module.exports = {cmd:cmd, accessLevel:0, exec:execute};
