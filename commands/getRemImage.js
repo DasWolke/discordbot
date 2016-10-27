7d9db01067807ce8f78371b58671ed25254d135f
Wolke <wolke@ram.moe>
2016-10-27 22:04:59 +0200

    Added Eggplant Added Flip Added more rem & lewd images and fixed some stuff added getShrug

--- commands/getRemImage.js
+++ commands/getRemImage.js
@@ -2,16 +2,20 @@
  * Created by julia on 02.10.2016.
  */
 var cmd = 'rem';
-var lewd = require('../config/lewd.json');
 var path = require('path');
 var logger = require('../utility/logger');
 var winston = logger.getT();
 var generalHelper = require('../utility/general');
+const fs = require('fs');
+const imagePath = '../remImages/';
 var execute = function (message) {
     //TODO remove when fixed
-    let number = generalHelper.random(0, lewd.rem.length-1);
-    message.channel.sendFile(path.join(__dirname, '../remImages/' + lewd.rem[number]), '', '\u200B').then(message => {
+    fs.readdir(path.join(__dirname, imagePath), (err, files) => {
+        if (err) return winston.error(err);
+        let number = generalHelper.random(0, files.length - 1);
+        message.channel.sendFile(path.join(__dirname, '../remImages/' + files[number]), '', '\u200B').then(message => {
 
-    }).catch(winston.info);
+        }).catch(winston.info);
+    });
 };
 module.exports = {cmd: cmd, accessLevel: 0, exec: execute, cat: 'misc'};
