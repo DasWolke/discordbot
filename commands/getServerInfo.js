69734f121a27a713b049d9b3fcabef8fe42fb1ae
Wolke <wolke@ram.moe>
2016-10-28 21:50:48 +0200

    Fixed an Error in Help Fixed an Error in getServerInfo

--- commands/getServerInfo.js
+++ commands/getServerInfo.js
@@ -15,7 +15,7 @@ var execute = function (message) {
         table
             .addRow(t('server-info.id', {lngs:message.lang}), message.guild.id)
             .addRow(t('server-info.name', {lngs:message.lang}), message.guild.name)
-            .addRow(t('server-info.member', {lngs:message.lang}), message.guild.members.size)
+            .addRow(t('server-info.member', {lngs: message.lang}), message.guild.memberCount)
             .addRow(t('server-info.text', {lngs:message.lang}), textChannels)
             .addRow(t('server-info.voice', {lngs:message.lang}), voiceChannels)
             .addRow(t('server-info.roles', {lngs:message.lang}), message.guild.roles.size)
