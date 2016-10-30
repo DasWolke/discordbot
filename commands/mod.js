0cf62b8aeb503d01aae20ef223447a44860157d7
Wolke <wolke@ram.moe>
2016-10-30 03:35:12 +0100

    Removed image dependencies rra is bae added new shorter links for invite

--- commands/mod.js
+++ commands/mod.js
@@ -16,6 +16,8 @@ var execute = function (message) {
                 case "lang":
                     lang.exec(message);
                     return;
+                case "rra":
+                    return;
                 // case "resolve":
                 //     message.reply(encodeURIComponent('麻枝 准×やなぎなぎ終わりの世界から'));
                 //     return;
