6fee5f0e80ed17013415b5361d06960b03c17afc
Wolke <wolke@ram.moe>
2016-11-03 18:39:11 +0100

    Started to make some admin commands for all shards

--- commands/setStatus.js
+++ commands/setStatus.js
@@ -5,21 +5,32 @@ var cmd = 'setStatus';
 var config = require('../config/main.json');
 var logger = require('../utility/logger');
 var winston = logger.getT();
+var Discord = require('discord.js');
 var execute = function (message) {
     if (message.author.id === config.owner_id) {
+        let shardUtil = new Discord.ShardClientUtil(message.botUser);
         let content = message.content.substr(message.prefix.length + cmd.length).trim();
         let contentSplit = content.split(']');
         let streamUrl = contentSplit.length > 1 ? contentSplit[1] : 'https://www.twitch.tv/daswolke_';
         if (content !== '') {
-            message.botUser.user.setGame(contentSplit[0], streamUrl).then(()=> {
+            winston.info(`this.user.setGame(${contentSplit[0]}, ${streamUrl})`);
+            shardUtil.broadcastEval(`this.user.setGame(${contentSplit[0]}, ${streamUrl})`).then(res => {
+                winston.info(res);
                 message.reply(`Set status to \`${contentSplit[0]}\` with url \`${streamUrl}\``);
             }).catch(winston.info);
+            // message.botUser.user.setGame(contentSplit[0], streamUrl).then(()=> {
+            //
+            // }).catch(winston.info);
         } else {
-            message.botUser.user.setGame(`!w.help | shard ${parseInt(message.shard_id) + 1}/${message.shard_count}`, 'https://www.twitch.tv/daswolke_').then().catch(winston.info).then(() => {
+            shardUtil.broadcastEval(`this.user.setGame(!w.help | shard ${parseInt(message.shard_id) + 1}/${message.shard_count}, https://www.twitch.tv/daswolke_`).then(res => {
+                winston.info(res);
                 message.reply(`Reset status back to \`!w.help | | shard ${parseInt(message.shard_id) + 1}/${message.shard_count}\``);
-            }).catch(err => {
-                winston.error(err)
-            });
+            }).catch(winston.info);
+            // message.botUser.user.setGame(`!w.help | shard ${parseInt(message.shard_id) + 1}/${message.shard_count}`, 'https://www.twitch.tv/daswolke_').then().catch(winston.info).then(() => {
+            //     message.reply(`Reset status back to \`!w.help | | shard ${parseInt(message.shard_id) + 1}/${message.shard_count}\``);
+            // }).catch(err => {
+            //     winston.error(err)
+            // });
         }
     }
 };
