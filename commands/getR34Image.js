9cf998b3b41091f448c2b0ceaa5c7d9d1c794539
Wolke <wolke@ram.moe>
2016-11-23 20:20:00 +0100

    uwu

--- commands/getR34Image.js
+++ commands/getR34Image.js
@@ -34,7 +34,7 @@ var r34 = function (message, messageSplit) {
             messageSearch = messageSearch + "+" + messageSplit[i];
         }
     }
-    request.get('http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags=' + messageSearch, function (error, response, body) {
+    request.get('https://rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' + messageSearch, function (error, response, body) {
         if (error) {
             message.reply('a error occured!');
         }
